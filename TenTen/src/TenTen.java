170b5d34cbfca3e57c192578d9b3138ac41cbae3
Leonel Delamo <ldelamojr@gmail.com>
2018-06-29 13:00:42 -0400

    Add comments from TenTenSolver to TenTen.

--- TenTen/src/TenTen.java
+++ TenTen/src/TenTen.java
@@ -194,11 +194,13 @@ public class TenTen {
         System.out.println("*************ROUND 1****************");
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // While loop 'defining' the life-span of the whole game from game start
+        // to game over
         while (gameOver == false) {
             if (round > 1) {
                 System.out.println("*************ROUND " + round + "****************");
             }
-
+            // Declare three variables to hold three pieces.
             int[][] thisRoundsZerothPiece = new int[5][5];
             int[][] thisRoundsFirstPiece = new int[5][5];
             int[][] thisRoundsSecondPiece = new int[5][5];
@@ -227,11 +229,12 @@ public class TenTen {
             // TenTen.deepClone(thisRoundsSecondPiece,
             // PieceReservoir[secondPiece]);
             //////////////////////////////////////////////////////////////////////////
-            // Randomly choosing pieces
+            // Instantiate three random pieces
             TenTen.deepClone(thisRoundsZerothPiece, TenTen.chooseRandomPieceFromReserve());
             TenTen.deepClone(thisRoundsFirstPiece, TenTen.chooseRandomPieceFromReserve());
             TenTen.deepClone(thisRoundsSecondPiece, TenTen.chooseRandomPieceFromReserve());
 
+            // Add them into "thisRoundsPieces" 3-D array
             thisRoundsPieces[0] = thisRoundsZerothPiece;
             thisRoundsPieces[1] = thisRoundsFirstPiece;
             thisRoundsPieces[2] = thisRoundsSecondPiece;
@@ -239,6 +242,7 @@ public class TenTen {
             TenTen.printGameBoard();
             System.out.println("SCORE " + score);
 
+            // While loop 'defining' the life-span of a round of three pieces.
             while (thisRoundsPieces[0][0][0] != 5 || thisRoundsPieces[1][0][0] != 5 || thisRoundsPieces[2][0][0] != 5) {
                 System.out.println("SCORE " + score);
                 TenTen.printGameBoard();
@@ -334,9 +338,20 @@ public class TenTen {
 
     public static void isGameOver(int[][][] thisRoundsPieces, int[][] gameBoard) {
         gameOver = true;
+        // These three booleans are used to break out to some level of the
+        // 5-nested for-loops. I'll give more details about each at some other
+        // time.
         Boolean brokeOutOfPiecesColumnIteration = false;
         Boolean brokeOutOfGameBoardColumnIteration = false;
         Boolean brokeOutOfGameBoardRowIteration = false;
+        // Definition: building block - Given a piece from the reservoir, the
+        // building blocks of the piece are those cells(coordinates) populated
+        // with a '1'.
+        // for each piece and
+        // for each row positioning of the piece and
+        // for each column positioning of the piece and
+        // for each row positioning of the building block and
+        // for each column positioning of the building block
         for (int pieceNumber = 0; pieceNumber <= 2; pieceNumber++) {
             brokeOutOfGameBoardRowIteration = false;
             brokeOutOfGameBoardColumnIteration = false;
@@ -365,6 +380,7 @@ public class TenTen {
                             break;
                         }
                         for (int j = 0; j <= 4; j++) {
+                            // Check if piece has already been used
                             if (thisRoundsPieces[pieceNumber][i][j] == 5) {
                                 // continue to next piece number
                                 brokeOutOfPiecesColumnIteration = true;
@@ -372,19 +388,41 @@ public class TenTen {
                                 break;
                             }
                             if (thisRoundsPieces[pieceNumber][i][j] == 1) {
+                                // Check if piece's current building block is
+                                // beyond the bottom of the gameboard.
                                 if (!(piecesRowPositionOnGameBoard + i <= 9)) {
                                     brokeOutOfGameBoardRowIteration = true;
                                     break;
                                 }
+
+                                // Check if piece's current building block is
+                                // beyond the right side of the gameboard.
                                 if (!(piecesColumnPositionOnGameBoard + j <= 9)) {
                                     brokeOutOfGameBoardColumnIteration = true;
                                     break;
                                 }
                             }
+                            // Check if current piece's current building block's
+                            // position is already occupied.
                             if (thisRoundsPieces[pieceNumber][i][j] == 1 && copyOfGameBoard[piecesRowPositionOnGameBoard + i][piecesColumnPositionOnGameBoard + j] == 1) {
                                 brokeOutOfPiecesColumnIteration = true;
                                 break;
                             }
+                            // If all of the buildings blocks of one of the
+                            // remaining pieces for this round all land inside
+                            // the game board for some (row position, column
+                            // position) on the game board,
+                            // then this means the game is not over yet and we
+                            // enter the below if-block and return back that the
+                            // game is not over.
+                            // Notice we only enter the below if-block if i and
+                            // j both equal 4 - that is we've checked all of the
+                            // possible building blocks of at least one of the
+                            // remaining pieces.
+                            // Otherwise we continue iterating through these
+                            // four loops until we reach i = j = 4 for some
+                            // (row position, column position) on the game
+                            // board.
                             if (i == 4 && j == 4) {
                                 gameOver = false;
                                 return;
@@ -394,6 +432,10 @@ public class TenTen {
                 }
             }
         }
+        // If for all remaining pieces and for all positions on the game board
+        // there exists some conflict of building blocks, then we never made it
+        // inside of the if statement above returning "gameOver = false;"
+        // And so this means none of the pieces fit and we end the game.
         System.out.println("*********\n*********\nGame Over\n*********\n*********\n");
     }
 
@@ -418,7 +460,9 @@ public class TenTen {
                     System.out.println("///////////////////////\nThis piece was played already\n//////////////////////");
                     return;
                 }
-
+                // If the current building block, (i,j), is out of bounds, then
+                // reset the copyOfGameBoard, copyScore, and copies of sums of
+                // rows and columns.
                 if (selectedPiece[i][j] == 1 && !(piecesRowPositionOnGameBoard + i <= 9 && piecesColumnPositionOnGameBoard + j <= 9)) {
                     TenTen.deepClone(copyOfGameBoard, gameBoard);
                     // copyOfGameBoard = gameBoard.clone();
@@ -430,7 +474,9 @@ public class TenTen {
                     System.out.println("//////////////////////\nOut of Bounds\n////////////////////////");
                     return;
                 }
-
+                // If selectedPiece's building block, (i,j), is placed on
+                // already taken spot, then reset copy variables as above
+                // if-statement
                 if (selectedPiece[i][j] == 1 && copyOfGameBoard[piecesRowPositionOnGameBoard + i][piecesColumnPositionOnGameBoard + j] == 1) {
                     TenTen.deepClone(copyOfGameBoard, gameBoard);
                     // copyOfGameBoard = gameBoard.clone();
@@ -442,15 +488,27 @@ public class TenTen {
                     System.out.println("/////////////////////\nSpot taken\n////////////////////////////");
                     return;
                 }
-
+                // If selected Piece's building block gets passed previous
+                // if-statements and the building block exists(i.e. is equal to
+                // 1), then place building block and add 1 to the
+                // sumOfRowsAndColumns in the proper row and proper column and
+                // increment the copyScore.
                 if (selectedPiece[i][j] == 1 && copyOfGameBoard[piecesRowPositionOnGameBoard + i][piecesColumnPositionOnGameBoard + j] == 0) {
                     copyOfGameBoard[piecesRowPositionOnGameBoard + i][piecesColumnPositionOnGameBoard + j] = copyOfGameBoard[piecesRowPositionOnGameBoard + i][piecesColumnPositionOnGameBoard + j] + selectedPiece[i][j];
                     copyOfSumOfRowsAndColumnsInGameBoard[0][piecesRowPositionOnGameBoard + i] = copyOfSumOfRowsAndColumnsInGameBoard[0][piecesRowPositionOnGameBoard + i] + 1;
                     copyOfSumOfRowsAndColumnsInGameBoard[1][piecesColumnPositionOnGameBoard + j] = copyOfSumOfRowsAndColumnsInGameBoard[1][piecesColumnPositionOnGameBoard + j] + 1;
                     copyScore = copyScore + 1;
+                    // If Sum of proper row equals 10, then indicate that row's
+                    // sum to be set to zero in the
+                    // copyOfSumsOfRowsAndColumnsToBeSetToZero variable by
+                    // setting proper row to 1(i.e. 1 means to reset to 0).
                     if (copyOfSumOfRowsAndColumnsInGameBoard[0][piecesRowPositionOnGameBoard + i] == 10) {
                         copyOfSumsOfRowsAndColumnsToBeSetToZero[0][piecesRowPositionOnGameBoard + i] = 1;
                     }
+                    // If Sum of proper column equals 10, then indicate that
+                    // column's sum to be set to zero in the
+                    // copyOfSumsOfRowsAndColumnsToBeSetToZero variable by
+                    // setting proper column to 1(i.e. 1 means to reset to 0).
                     if (copyOfSumOfRowsAndColumnsInGameBoard[1][piecesColumnPositionOnGameBoard + j] == 10) {
                         copyOfSumsOfRowsAndColumnsToBeSetToZero[1][piecesColumnPositionOnGameBoard + j] = 1;
                     }
@@ -458,20 +516,51 @@ public class TenTen {
                 }
             }
         }
+        // Below is further logic for adding to the score and further logic for
+        // resetting the sumOfRowsAndColumsInGameBoard's proper row and column
+        // to 0.
+
+        // TODO See if I can change this giant if-else block by removing the
+        // if-else completely and using a return statement just before the
+        // else-block. The reason I think this could be possible is because I
+        // don't think we'll reach this code if the conditional is true anyway.
+        // Maybe a better way to say it is - If we reach the below code then it
+        // must have been the case that "breakOutOfOuterLoop" IS INDEED false.
+        // i.e. we don't need to check if it's false if it's always false when
+        // we arrive here.
         if (breakOutOfOuterLoop == false) {
+            // Set indicate the piece to be unplayabe as the round continues
+            // since it has been placed on the gameboard already.
             selectedPiece[0][0] = 5;
-            TenTen.deepClone(gameBoard, copyOfGameBoard);
+            // update gameBoard with the latest building blocks added.
+            TenTenSolver.deepClone(gameBoard, copyOfGameBoard);
             // gameBoard = copyOfGameBoard.clone();
+
+            // For all rows, if the row is now filled with building blocks, then
+            // reset every entry in that row to zero i.e. delete the building
+            // blocks in that row.
+            // For all Columns, if the Columns is now filled with building
+            // blocks, then reset every entry in that column to zero i.e. delete
+            // the building blocks in that columns.
+            // Further comments inside.
             for (int i = 0; i <= 1; i++) {
                 for (int j = 0; j <= 9; j++) {
                     if (copyOfSumOfRowsAndColumnsInGameBoard[i][j] == 10) {
-                        // What I should do is merely record which rows and
+                        // TODO What I should do is merely record which rows and
                         // columns sum to 10 and then after I know all of them,
                         // delete them all together.
+
+                        // If we're deleting a row that has been filled, then we
+                        // hold the row constant and iterate through all the
+                        // columns resetting back to zero as we go along.
                         if (i == 0) {
                             for (int columnIndex = 0; columnIndex <= 9; columnIndex++) {
                                 gameBoard[j][columnIndex] = 0;
                             }
+                            // Otherwise, if we're deleting a column, then we
+                            // hold the column constant and iterate through the
+                            // rows resetting the entries in the column back to
+                            // zero.
                         } else {
                             for (int rowIndex = 0; rowIndex <= 9; rowIndex++) {
                                 gameBoard[rowIndex][j] = 0;
@@ -480,6 +569,7 @@ public class TenTen {
                     }
                 }
             }
+            // Logic for adding
             int numberOfRowsDeleted = 0;
             int numberOfColumnsDeleted = 0;
             for (int rowsAndColumns = 0; rowsAndColumns <= 1; rowsAndColumns++) {
@@ -495,7 +585,8 @@ public class TenTen {
                     }
                 }
             }
-
+            // Add the triangle number to the score. 10*Ti, where i is the
+            // number of rows and columns deleted.
             if (numberOfRowsDeleted + numberOfColumnsDeleted > 0) {
                 int totalRowsAndColumnsDeleted = numberOfRowsDeleted + numberOfColumnsDeleted;
                 for (int i = totalRowsAndColumnsDeleted; i >= 1; i--) {
@@ -507,8 +598,11 @@ public class TenTen {
             // TODO the "if" below needs to also check "OR numberOfRowsDeleted >
             // 0", perhaps in same for loop and if conditional or separate, idk
             // yet.
-            // Account for subtracting 1 from a row when a column was deleted or
-            // any number of column deletions
+            // Account for subtracting 1 building block from EVERY row when a
+            // column was deleted or
+            // any number of column deletions. i.e. n column deletions implies
+            // to subtract n from the total number of building blocks in EVERY
+            // row.
             if (numberOfColumnsDeleted > 0) {
                 for (int row = 0; row <= 9; row++) {
                     for (int subtractionIteration = 1; subtractionIteration <= numberOfColumnsDeleted; subtractionIteration++) {
@@ -518,6 +612,11 @@ public class TenTen {
                     }
                 }
             }
+            // Account for subtracting 1 building block from EVERY column when a
+            // row was deleted or
+            // any number of row deletions. i.e. n row deletions implies
+            // to subtract n from the total number of building blocks in EVERY
+            // column.
             if (numberOfRowsDeleted > 0) {
                 for (int column = 0; column <= 9; column++) {
                     for (int subtractionIteration = 1; subtractionIteration <= numberOfRowsDeleted; subtractionIteration++) {
@@ -527,10 +626,15 @@ public class TenTen {
                     }
                 }
             }
+            // Copy the attempted piece's placement resulting scores and other
+            // variables to be the most recent successful change.
             numberOfColumnsDeleted = 0;
             numberOfRowsDeleted = 0;
             TenTen.deepClone(sumOfRowsAndColumnsInGameBoard, copyOfSumOfRowsAndColumnsInGameBoard);
             TenTen.deepClone(copyOfSumsOfRowsAndColumnsToBeSetToZero, sumsOfRowsAndColumnsToBeSetToZero);
+            // If we did not successfully place a piece onto the game board then
+            // abort this attempt and reset all temporary variables to the last
+            // successful changes.
         } else {
             TenTen.deepClone(copyOfGameBoard, gameBoard);
             TenTen.deepClone(copyOfSumOfRowsAndColumnsInGameBoard, sumOfRowsAndColumnsInGameBoard);
